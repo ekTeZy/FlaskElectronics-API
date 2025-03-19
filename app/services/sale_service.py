972cdcad995c5142b9f1b7335175292efd6ac223
Egor <kamolikov.en@gmail.com>
2025-03-19 23:43:24 +0300

    Оптимизация SaleService: - Убрано дублирование вызова SaleRepository.get_total_sales(). - Исправлены вызовы методов CacheManager (убран cls).

--- app/services/sale_service.py
+++ app/services/sale_service.py
@@ -1,5 +1,6 @@
 from typing import List
 from app.repositories.sale_repo import SaleRepository
+from app.utils.cache import CacheManager
 from werkzeug.exceptions import (
     BadRequest
 )
@@ -23,17 +24,35 @@ class SaleService:
 
     @staticmethod
     def get_total_sales(start_date: str, end_date: str) -> float:
+        cache_key = f"total_sales:{start_date}:{end_date}"
+        cached_data = CacheManager.get(cache_key)
+
+        if cached_data is not None:
+            return cached_data
+
         start_date_dt = SaleService.parse_date(start_date)
         end_date_dt = SaleService.parse_date(end_date)
 
-        return SaleRepository.get_total_sales(start_date_dt, end_date_dt)
+        total_sales = SaleRepository.get_total_sales(
+            start_date_dt, end_date_dt)
+        CacheManager.set(cache_key, total_sales)
+
+        return total_sales
 
     @staticmethod
     def get_top_selling_products(start_date: str, end_date: str, limit: int) -> List[dict]:
+        cache_key = f"top_products:{start_date}:{end_date}:{limit}"
+        cached_data = CacheManager.get(cache_key)
+
+        if cached_data is not None:
+            return cached_data
+
         start_date_dt = SaleService.parse_date(start_date)
         end_date_dt = SaleService.parse_date(end_date)
 
-        if not isinstance(limit, int) or limit <= 0:
-            raise BadRequest("Лимит должен быть положительным целым числом.")
+        top_selling_products = SaleRepository.get_top_selling_products(
+            start_date_dt, end_date_dt, limit
+        )
 
-        return SaleRepository.get_top_selling_products(start_date_dt, end_date_dt, limit)
+        CacheManager.set(cache_key, top_selling_products)
+        return top_selling_products
