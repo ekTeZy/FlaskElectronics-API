d402db8f1e5bf549cb1ef0dad8c605cc1fadf417
Egor <kamolikov.en@gmail.com>
2025-03-19 18:55:50 +0300

    Обновлён ProductService

---
+++ app/services/product_service.py
@@ -0,0 +1,9 @@
+# from app.repositories.product_repo import ProductRepository
+
+
+# class ProductService:
+
+#     @staticmethod
+#     def get_all_products() -> list[dict]:
+#         products = ProductRepository.get_all()
+#         return [p.to_dict() for p in products]
