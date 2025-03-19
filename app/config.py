a8689d8d3878138aabfb47c82fab5fffdc1e2559
Egor <kamolikov.en@gmail.com>
2025-03-19 17:26:54 +0300

    Перемещена папка migrations в app/, обновлен config.py

--- app/config.py
+++ app/config.py
@@ -10,3 +10,6 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
     CACHE_TYPE: str = "simple"
     CACHE_DEFAULT_TIMEOUT: int = 300
+
+    basedir: str = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_MIGRATE_REPO: str = os.path.join(basedir, 'migrations')
