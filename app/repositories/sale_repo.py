4aa138780b8f16707abf413fefb58da2e9d240ed
Egor <kamolikov.en@gmail.com>
2025-03-19 21:19:49 +0300

    Добавлен метод get_top_selling_products() в SaleRepository: получение топ-N товаров за период

--- app/repositories/sale_repo.py
+++ app/repositories/sale_repo.py
@@ -1,6 +1,7 @@
 from typing import List
 from app.database import db
 from app.models.sale import Sale
+from app.models.product import Product
 from datetime import datetime
 from sqlalchemy import func
 
@@ -17,3 +18,18 @@ class SaleRepository:
             Sale.date.between(start_date, end_date)).scalar()
 
         return total_sales
+
+    @staticmethod
+    def get_top_selling_products(start_date: datetime, end_date: datetime, limit: int) -> List[dict]:
+        top_selling_products = (
+            db.session.query(Product.name, func.sum(
+                Sale.quantity).label("total_quantity"))
+            .join(Sale, Product.id == Sale.product_id)
+            .filter(Sale.date.between(start_date, end_date))
+            .group_by(Product.name)
+            .order_by(func.sum(Sale.quantity).desc())
+            .limit(limit)
+            .all()
+        )
+
+        return [{"product": product[0], "total_quantity": product[1]} for product in top_selling_products]
