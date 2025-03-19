1d442d1f61cc44503b2e1c20bc03b292ab9f6397
Egor <kamolikov.en@gmail.com>
2025-03-19 17:43:29 +0300

    Исправлена инициализация моделей в database.py для миграций

--- app/database.py
+++ app/database.py
@@ -6,7 +6,11 @@ from app.config import Config
 db = SQLAlchemy()
 migrate = Migrate()
 
+
 def init_db(app: Flask) -> None:
     app.config.from_object(Config)
+
+    from app.models import category, product, sale
+
     db.init_app(app)
     migrate.init_app(app, db)
