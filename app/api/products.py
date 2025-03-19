b9d930d3be88c93137b54d50fc1f66659242151e
Egor <kamolikov.en@gmail.com>
2025-03-19 20:18:49 +0300

    Реализованы API-маршруты для продуктов: - GET /api/products: Получение всех продуктов. - POST /api/products: Создание нового продукта. - GET /api/products/<id>: Получение продукта по ID. - PUT /api/products/<id>: Обновление продукта. - DELETE /api/products/<id>: Удаление продукта. - Обработаны исключения , , .

---
+++ app/api/products.py
@@ -0,0 +1,68 @@
+from flask import Blueprint, jsonify, request
+from werkzeug.exceptions import BadRequest, NotFound, InternalServerError
+from app.services.product_service import ProductService
+
+products_bp = Blueprint("products", __name__, url_prefix="/products")
+
+@products_bp.route("/", methods=["GET"])
+def get_all_products():
+    products = ProductService.get_all_products()
+
+    return jsonify(products), 200
+
+@products_bp.route("/<int:product_id>", methods=["GET"])
+def get_product_by_id(product_id: int):
+    try:
+        product = ProductService.get_product_by_id(product_id)
+
+        return jsonify(product), 200
+
+    except NotFound as e:
+        return jsonify({"error": str(e)}), 404
+
+@products_bp.route("/", methods=["POST"])
+def create_product():
+    try:
+        data = request.get_json()
+        new_product = ProductService.create_product(data)
+
+        return jsonify(new_product), 201
+
+    except BadRequest as e:
+        return jsonify({"error": str(e)}), 400
+
+    except InternalServerError as e:
+        return jsonify({"error": str(e)}), 500
+
+@products_bp.route("/<int:product_id>", methods=["PUT"])
+def update_product(product_id: int):
+    try:
+        data = request.get_json()
+        updated_product = ProductService.update_product_by_id(product_id, data)
+
+        return jsonify(updated_product), 200
+
+    except NotFound as e:
+        return jsonify({"error": str(e)}), 404
+
+    except BadRequest as e:
+        return jsonify({"error": str(e)}), 400
+
+    except InternalServerError as e:
+        return jsonify({"error": str(e)}), 500
+
+@products_bp.route("/<int:product_id>", methods=["DELETE"])
+def delete_product(product_id: int):
+    try:
+        response = ProductService.delete_product_by_id(product_id)
+
+        return jsonify(response), 200
+
+    except NotFound as e:
+        return jsonify({"error": str(e)}), 404
+
+    except BadRequest as e:
+        return jsonify({"error": str(e)}), 400
+
+    except InternalServerError as e:
+        return jsonify({"error": str(e)}), 500
